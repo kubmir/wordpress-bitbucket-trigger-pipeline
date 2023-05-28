84ec4d323bb12e43d42ddaf55150978bc0d49bd4
miroslavkubus <433645@muni.cz>
2023-05-28 14:15:36 +0200

    feat: rework auth to use tokens as basic auth is deprecated

--- bitbucket-trigger-pipeline.php
+++ bitbucket-trigger-pipeline.php
@@ -1,13 +1,13 @@
 <?php
 /*
 Plugin Name: BitBucket Trigger Pipeline
-Plugin URI: https://github.com/kmturley/wordpress-bitbucket-trigger-pipeline
-Description: Wordpress Plugin triggers a BitBucket Pipeline when user publishes a post (using environment variables BITBUCKET_PROJECT, BITBUCKET_USERNAME and BITBUCKET_PASSWORD)
-Version: 0.1
-Author: Kim T
-Author URI: https://github.com/kmturley
+Plugin URI: https://github.com/kubmir/wordpress-bitbucket-trigger-pipeline
+Description: Wordpress Plugin triggers a BitBucket Pipeline when user publishes a post (using environment variables BITBUCKET_PROJECT, BITBUCKET_WORKSPACE and BITBUCKET_API_KEY)
+Version: 0.2
+Author: kubmir (forked from: Kim T)
+Author URI: https://github.com/kubmir
 License: GPL
-Copyright: Kim T
+Copyright: kubmir (forked from: Kim T)
 */
 
 add_action('publish_page', 'publish_static_hook');
@@ -15,13 +15,13 @@ add_action('publish_post', 'publish_static_hook');
 
 function publish_static_hook($id) {
 
+  $bitbucket_workspace = get_option('option_workspace');
   $bitbucket_project = get_option('option_project');
   $bitbucket_branch = get_option('option_branch');
-  $bitbucket_username = get_option('option_username');
-  $bitbucket_app_password = get_option('option_password');
+  $bitbucket_api_key = get_option('option_api_key');
 
   // if variables are set, then trigger static build
-  if ($bitbucket_project && $bitbucket_branch && $bitbucket_username && $bitbucket_app_password) {
+  if ($bitbucket_workspace && $bitbucket_project && $bitbucket_branch && $bitbucket_api_key) {
     $data = array(
       'target' => array(
         'ref_type' => 'branch',
@@ -31,15 +31,15 @@ function publish_static_hook($id) {
     );
     // Based on Atlassian documentation
     // https://developer.atlassian.com/bitbucket/api/2/reference/resource/repositories/%7Bworkspace%7D/%7Brepo_slug%7D/pipelines/
-    $response = wp_remote_post('https://api.bitbucket.org/2.0/repositories/'.$bitbucket_username.'/'.$bitbucket_project.'/pipelines/', array(
+    $response = wp_remote_post('https://api.bitbucket.org/2.0/repositories/'.$bitbucket_workspace.'/'.$bitbucket_project.'/pipelines/', array(
       'body' => json_encode($data),
       'headers' => array(
-        'Authorization' => 'Basic '.base64_encode($bitbucket_username.':'.$bitbucket_app_password),
+        'Authorization' => 'Bearer '.$bitbucket_api_key,
         'Content-Type' => 'application/json'
       ),
     ));
     // for debugging
-    // echo '<pre>https://api.bitbucket.org/2.0/repositories/'.$bitbucket_username.'/'.$bitbucket_project.'/pipelines/</pre>';
+    // echo '<pre>https://api.bitbucket.org/2.0/repositories/'.$bitbucket_workspace.'/'.$bitbucket_project.'/pipelines/</pre>';
     // echo '<pre>'.print_r($data, true).'</pre>';
     // echo '<pre>'.print_r($response, true).'</pre>';
   }
@@ -52,6 +52,17 @@ function my_general_section() {
     'Bitbucket Settings',
     'my_section_options_callback',
     'general'
+  );
+
+  add_settings_field(
+    'option_workspace',
+    'BITBUCKET WORKSPACE',
+    'my_textbox_callback',
+    'general',
+    'my_settings_section',
+    array(
+        'option_workspace'
+    )
   );
 
   add_settings_field(
@@ -77,31 +88,20 @@ function my_general_section() {
   );
 
 	add_settings_field(
-    'option_username',
-    'BITBUCKET USERNAME',
+    'option_api_key',
+    'BITBUCKET API KEY',
     'my_textbox_callback',
     'general',
     'my_settings_section',
     array(
-        'option_username'
-    )
-  );
-
-	add_settings_field(
-    'option_password',
-    'BITBUCKET APP PASSWORD',
-    'my_password_callback',
-    'general',
-    'my_settings_section',
-    array(
-        'option_password'
+        'option_api_key'
     )
   );
 
   register_setting('general','option_project', 'esc_attr');
 	register_setting('general','option_branch', 'esc_attr');
-	register_setting('general','option_username', 'esc_attr');
-	register_setting('general','option_password', 'esc_attr');
+	register_setting('general','option_workspace', 'esc_attr');
+	register_setting('general','option_api_key', 'esc_attr');
 }
 
 function my_section_options_callback() {
@@ -112,9 +112,3 @@ function my_textbox_callback($args) {
     $option = get_option($args[0]);
     echo '<input type="text" id="'. $args[0] .'" name="'. $args[0] .'" value="' . $option . '" />';
 }
-
-function my_password_callback($args) {
-    $option = get_option($args[0]);
-    echo '<input type="password" id="'. $args[0] .'" name="'. $args[0] .'" value="' . $option . '" />';
-}
-?>
